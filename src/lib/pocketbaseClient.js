19d513755d03de0aff3038e8f2b222fcb181079e
giverhamtech <hassanadelajagiver@gmail.com>
2026-06-18 09:12:57 +0100

    Connected Firebase

--- src/lib/pocketbaseClient.js
+++ src/lib/pocketbaseClient.js
@@ -1,9 +1,16 @@
-import Pocketbase from 'pocketbase';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const POCKETBASE_API_URL = '/hcgi/platform';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG0IzRUN831nw8j273zRbDmJC0Ysa7Buk",
+  authDomain: "giver-recording-studio.firebaseapp.com",
+  projectId: "giver-recording-studio",
+  storageBucket: "giver-recording-studio.firebasestorage.app",
+  messagingSenderId: "365116469350",
+  appId: "1:365116469350:web:f579fd70fea7e80693fda7"
+};
 
-const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
-
-export default pocketbaseClient;
-
-export { pocketbaseClient };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
